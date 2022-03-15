550dd136acbb9bebc8e118f47eb1040f696422ef
svitalis123 <vitalismutwiri@gmail.com>
2022-03-15 02:58:42 -0700

    g

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet -
+ * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
-	int i=0;
+	int i = 0;
+
 	while (i < 10)
 	{
 	char alp = 'a';
@@ -16,14 +17,10 @@ void print_alphabet_x10(void)
 	while (alp <= 'z')
 	{
 		_putchar (alp);
-		alp++;
-		
+		alp++;	
 	}
 	_putchar ('\n');
 
 	i++;
 	}
-	
-
-
 }
