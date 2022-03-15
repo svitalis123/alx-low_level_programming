254033c5ca0927b0cfcbcc4399ab0480c03b312f
svitalis123 <vitalismutwiri@gmail.com>
2022-03-15 02:43:37 -0700

    j

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet -
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i=0;
+	while (i <= 10)
+	{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+		
+	}
+	_putchar ('\n');
+
+	i++;
+	}
+	
+
+
+}
