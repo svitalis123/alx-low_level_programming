c356f7495695e7382760d5b7fd9e4501e2b99c7c
svitalis123 <vitalismutwiri@gmail.com>
2022-03-16 02:46:59 -0700

    s

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
 srand(time(0));
 /* your code goes there */
-if (n > 0)
+if (i > 0)
 {
-	printf("%d is positive\n", n);
+	printf("%d is positive\n", i);
 }
-if (n < 0)
+if (i < 0)
 {
-	printf("%d is negative\n", n);
+	printf("%d is negative\n", i);
 }
-if (n == 0)
+if (i == 0)
 {
-	printf("%d is zero\n", n);
+	printf("%d is zero\n", i);
 }
 
 }
