a8dae897e41d1de7ceddaff8a820b44355ed3ac7
svitalis123 <vitalismutwiri@gmail.com>
2022-03-22 22:02:06 -0700

    strcat

--- main.h
+++ main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -12,3 +18,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
+#end if
