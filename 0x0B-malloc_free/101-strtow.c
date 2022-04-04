b6c6a81b71cb156672d8bf5c0ef710f99692eccb
svitalis123 <vitalismutwiri@gmail.com>
2022-04-04 02:09:01 -0700

    try

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -44,41 +44,37 @@ char **strtow(char *str)
 
 	while (*str)
 	{
-		while (*str == ' ' && *str)
-		{
-			str++;
-		}
-		len = 0;
-
-		while (*(str + len) != ' ' && *(str + len))
-		{
-			len += 1;
-		}
+	while (*str == ' ' && *str)
+	{
+		str++;
+	}
+	len = 0;
+	while (*(str + len) != ' ' && *(str + len))
+	{
 		len += 1;
-		col = malloc(sizeof(char) * len);
-
-		if (!col)
+	}
+	len += 1;
+	col = malloc(sizeof(char) * len);
+	if (!col)
+	{
+		for (k = j - 1; k >= 0; k--)
 		{
-			for (k = j - 1; k >= 0; k--)
-			{
-				free(f[k]);
-			}
-			free(f);
-			return (NULL);
+			free(f[k]);
 		}
-
-		for (k = 0; k < (len - 1);  k++)
-		{
-			*(col + k) = *(str++);
-		}
-		*(col + k) = '\0';
-		*(f + j) = col;
-
-		if (j < (count - 1))
-		{
-			j++;
-		}
+		free(f);
+		return (NULL);
+	}
+	for (k = 0; k < (len - 1);  k++)
+	{
+		*(col + k) = *(str++);
+	}
+	*(col + k) = '\0';
+	*(f + j) = col;
+	if (j < (count - 1))
+	{
+		j++;
+	}
 	}
 	*(f + j) = NULL;
 	return (f);
-} /*yes*/
+}
